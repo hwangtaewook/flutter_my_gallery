ab2819807ee5a8eb72182777609a6bb9ed295699
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-04-09 00:18:13 +0900

    feat : Image Picker로 사진 선택

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,40 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ,
+      home: const MyGalleryApp(),
+    );
+  }
+}
+
+class MyGalleryApp extends StatefulWidget {
+  const MyGalleryApp({super.key});
+
+  @override
+  State<MyGalleryApp> createState() => _MyGalleryAppState();
+}
+
+class _MyGalleryAppState extends State<MyGalleryApp> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? images;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('전자액자'),
+      ),
+      // body: ,
     );
   }
 }
