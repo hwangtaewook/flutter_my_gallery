82b34a45ef740d56abe19cb021522a5607b3cd87
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-04-09 00:33:03 +0900

    feat : PageView로 화면 슬라이드 구현

--- lib/main.dart
+++ lib/main.dart
@@ -56,22 +56,29 @@ class _MyGalleryAppState extends State<MyGalleryApp> {
           ? const Center(
               child: Text('No data'),
             )
-          : FutureBuilder<Uint8List>(
-              future: images![0].readAsBytes(),
-              builder: (context, snapshot) {
-                final data = snapshot.data;
+          : PageView(
+              children: images!.map(
+                (image) {
+                  return FutureBuilder<Uint8List>(
+                    future: image.readAsBytes(),
+                    builder: (context, snapshot) {
+                      final data = snapshot.data;
 
-                if (data == null ||
-                    snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
+                      if (data == null ||
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return Image.memory(
+                        data,
+                        width: double.infinity,
+                      );
+                    },
                   );
-                }
-                return Image.memory(
-                  data,
-                  width: double.infinity,
-                );
-              }),
+                },
+              ).toList(),
+            ),
     );
   }
 }
